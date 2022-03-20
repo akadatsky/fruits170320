24166c4046e50de161b05cc6533ffc8139074389
Iryna Maslianko <persocom01@gmail.com>
2022-03-20 20:56:04 +0200

    lesson 11

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -3,19 +3,16 @@ package com.akadatsky;
 public class Main {
 
     public static void main(String[] args) {
-        Fruit greenApple = new Fruit();
-        greenApple.setColor(Color.GREEN);
-        greenApple.setType(Fruit.Type.APPLE);
-        greenApple.setWeight(216);
-        greenApple.setPricePerKg(14.90);
-
-        Fruit redApple = new Fruit(262, 20.90);
-        Fruit orange;
-        Fruit carrot;
+        Fruit greenApple = new Fruit(Color.GREEN, Fruit.Type.APPLE, 250, 15);
+        Fruit redApple = new Fruit(Color.RED, Fruit.Type.APPLE, 250, 15);
+        Fruit orange = new Fruit(Color.ORANGE, Fruit.Type.ORANGE, 120, 40);
+        Fruit carrot = new Fruit(Color.ORANGE, Fruit.Type.CARROT, 80, 25);
 
         CashMachine cashMachine = new CashMachine();
         cashMachine.add(greenApple);
         cashMachine.add(redApple);
+        cashMachine.add(orange);
+        cashMachine.add(carrot);
         cashMachine.printBill();
     }
 
